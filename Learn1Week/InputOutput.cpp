777a76eeb1200ad5074e2c25089438a1c0fdb456
ShiniDev <markjoefreylaurente@gmail.com>
2021-02-09 16:38:27 +0800

    Removed some parenthesis for cleaner and shorter code

--- Learn1Week/InputOutput.cpp
+++ Learn1Week/InputOutput.cpp
@@ -117,24 +117,18 @@ void numerical_sequence()
     int num3 = -3;
     cin >> num1 >> num2 >> num3;
     cout << endl;
-    if(num1<num2 && num2<num3){
+    if(num1<num2 && num2<num3)
         cout << num1 << ", " << num2 << ", " << num3 << "\n";
-    }
-    else if(num2<num1 && num1<num3){
+    else if(num2<num1 && num1<num3)
         cout << num2 << ", " << num1 << ", " << num3 << "\n";
-    }
-    else if(num3<num2 && num2<num1){
+    else if(num3<num2 && num2<num1)
         cout << num3 << ", " << num2 << ", " << num1 << "\n";
-    }
-    else if(num3<num1 && num1<num2){
+    else if(num3<num1 && num1<num2)
         cout << num3 << ", " << num1 << ", " << num2 << "\n";
-    }
-    else if(num2<num3 && num3<num1){
+    else if(num2<num3 && num3<num1)
         cout << num2 << ", " << num3 << ", " << num1 << "\n";
-    }
-    else if(num1<num3 && num3<num2){
+    else if(num1<num3 && num3<num2)
         cout << num1 << ", " << num3 << ", " << num2 << "\n";
-    }
 }
 void string_sequence()
 {
